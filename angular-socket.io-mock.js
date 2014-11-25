bc3236ebb4d5b658a74e7ee2bbee76b6319f366a
Isaac Johnson <iojohnso@gmail.com>
2014-11-25 11:34:40 -0600

    Update angular-socket.io-mock.js

--- angular-socket.io-mock.js
+++ angular-socket.io-mock.js
@@ -1,44 +1,49 @@
 /* global angular: false */
-var ng = angular.module("btford.socket-io",[])
-ng.provider("socketFactory",function(){
-  this.$get = function($rootScope){
+var ng = angular.module("btford.socket-io", []);
+ng.provider("socketFactory", function () {
+	"use strict";
 
-    return function() {
-      var obj = {}
-      obj.events = {}
-      obj.emits = {}
+	this.$get = function ($rootScope) {
 
-      // intercept 'on' calls and capture the callbacks
-      obj.on = function(eventName, callback){
-        if(!this.events[eventName]) this.events[eventName] = []
-        this.events[eventName].push(callback)
-      }
+		return function () {
 
-      // intercept 'emit' calls from the client and record them to assert against in the test
-      obj.emit = function(eventName){
-        var args = Array.prototype.slice.call(arguments,1)
+			var obj = {};
+			obj.events = {};
+			obj.emits = {};
 
-        if(!this.emits[eventName])
-          this.emits[eventName] = []
-        this.emits[eventName].push(args)
-      }
+			// intercept 'on' calls and capture the callbacks
+			obj.on = function (eventName, callback) {
 
-      //simulate an inbound message to the socket from the server (only called from the test)
-      obj.receive = function(eventName){
-        var args = Array.prototype.slice.call(arguments,1)
+				if (!this.events[eventName]) {
+					this.events[eventName] = [];
+				}
+				this.events[eventName].push(callback);
+			};
 
-        if(this.events[eventName]){
+			// intercept 'emit' calls from the client and record them to assert against in the test
+			obj.emit = function (eventName) {
 
-          angular.forEach(this.events[eventName], function(callback){
-            $rootScope.$apply(function() {
+				var args = Array.prototype.slice.call(arguments, 1);
+				if (!this.emits[eventName]) {
+					this.emits[eventName] = [];
+				}
+				this.emits[eventName].push(args);
+			};
 
-              callback.apply(this, args)
-            })
-          })
-        }
-      }
+			//simulate an inbound message to the socket from the server (only called from the test)
+			obj.receive = function (eventName) {
 
-      return obj
-    };
-  }
-})
+				var args = Array.prototype.slice.call(arguments, 1);
+				if (this.events[eventName]) {
+					angular.forEach(this.events[eventName], function (callback) {
+						$rootScope.$apply(function () {
+							callback.apply(this, args);
+						})
+					})
+				}
+			};
+
+			return obj;
+		};
+	}
+});
